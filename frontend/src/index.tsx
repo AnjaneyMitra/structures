e29a5ad791aa019c612e3a460fc99f56f76be422
AnjaneyMitra <mitraanjaney@gmail.com>
2025-07-21 18:46:40 +0530

    fixed font import

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,7 +10,6 @@ import '@fontsource/inter/700.css';
 import '@fontsource/space-grotesk/400.css';
 import '@fontsource/space-grotesk/600.css';
 import '@fontsource/space-grotesk/700.css';
-import '@fontsource/space-grotesk/800.css';
 import '@fontsource/ibm-plex-sans/400.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/jetbrains-mono/400.css';
